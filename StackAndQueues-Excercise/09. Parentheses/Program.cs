bf3b743ee989969c2a8da6e50467658290a80ce9
Bu7ers <Antoanlukanov@gmail.com>
2021-01-18 21:43:55 +0200

    .

---
+++ StackAndQueues-Excercise/09. Parentheses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace Parentheses
+	{
+	class Program
+		{
+		static void Main (string[] args)
+		{
+			string input = Console.ReadLine();
+			Stack<char> stack = new Stack<char>();
+			bool istrue = true;
+			foreach (char item in input)
+			{
+				if (item == '(' || item == '{' || item == '[')
+				{
+					stack.Push(item);
+				}
+				else
+				{
+					if (item == ')' && stack.Pop() == '(')
+					{
+					  continue;
+					}
+					else if (item == '}' && stack.Pop() == '{')
+					{
+						continue;
+					}
+					else if (item == ']' && stack.Pop() == '[')
+					{
+						continue;
+					}
+					else
+					{
+						istrue = false;
+						Console.WriteLine("NO");
+						break;
+					}
+					}
+
+				
+			}
+			if (istrue == true)
+			{
+				Console.WriteLine("YES");
+			}
+			}
+		}
+	}
---
+++ StackAndQueues-Excercise/10. Crossroads/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Crossroads
+	{
+	class Program
+		{
+		static void Main (string[] args)
+		{
+			int greenLightDuration = int.Parse(Console.ReadLine());
+			int freeWindow = int.Parse(Console.ReadLine());
+			Queue<string> queue = new Queue<string>();
+			int totalpass = 0;
+			bool crash = false;
+			while (true)
+			{
+				string input = Console.ReadLine();
+				
+				if (input.ToLower() == "end")
+				{
+					Console.WriteLine($"Everyone is safe.");
+					Console.WriteLine($"{totalpass} total cars passed the crossroads.");
+					break;
+				}
+				else if (input.ToLower() == "green")
+				{
+					int currentwindow = greenLightDuration;
+					while (currentwindow > 0 && queue.Count > 0)
+					{
+						int currentcarsize = queue.Peek().Length;
+						string currentcar = queue.Dequeue();
+						if (currentcarsize > currentwindow + freeWindow)
+						{
+							Console.WriteLine($"A crash happened!");
+							Console.WriteLine($"{currentcar} was hit at {currentcar[freeWindow + currentwindow]}.");
+							crash = true;
+
+							break;
+						}
+						currentwindow -= currentcarsize;
+
+
+						totalpass += 1;
+					}
+
+					if (crash == true)
+					{
+						break;
+					}
+
+					continue;
+				}
+				queue.Enqueue(input);
+
+				}
+		}
+		}
+	}
+
