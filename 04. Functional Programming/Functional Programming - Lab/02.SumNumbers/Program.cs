4ba1410009e98b6216a940dfa166d839b9770a99
Bu7ers <Antoanlukanov@gmail.com>
2021-01-31 17:15:23 +0200

    Lab

---
+++ 04. Functional Programming/Functional Programming - Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace FunctionalProgramming
+	{
+	class Program
+		{
+		static void Main (string[] args)
+		{
+			PrintSumAndCount(x => int.Parse(x), a => a.Length, array => array.Sum());
+		}
+
+		static void PrintSumAndCount(Func<string, int> parser,
+			Func<int[], int> CountGetter,
+			Func<int[], int> SumCalc)
+		{
+			int[] array =
+				Console.ReadLine()
+					.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+					.Select(parser)
+					.ToArray();
+			Console.WriteLine(CountGetter(array));
+			Console.WriteLine(SumCalc(array));
+
+			}
+		}
+	}
---
+++ 04. Functional Programming/Functional Programming - Lab/05.FilterByAge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FilterByAge
+	{
+	class persons
+		{
+		public string name { get; set; }
+		public int age { get; set; }
+
+		}
+
+	class Program
+		{
+		static void Main (string[] args)
+			{
+			int n = int.Parse(Console.ReadLine());
+			persons[] people = new persons[n];
+
+			for (int i = 0; i < n; i++)
+				{
+				var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+				people[i] = new persons();
+				people[i].name = input[0];
+				people[i].age = int.Parse(input[1]);
+				}
+
+			string filter = Console.ReadLine();
+			int filterAge = int.Parse(Console.ReadLine());
+			Func<persons, bool> condition = GetAgeCondition(filter, filterAge);
+			Func<persons, string> formatter = GetFormatter(Console.ReadLine());
+			printpeople(people, condition, formatter);
+			}
+		static Func<persons, bool> GetAgeCondition (string filter, int filterAge)
+			{
+			switch (filter)
+				{
+
+				case "younger": return p => p.age < filterAge;
+				case "older": return p => p.age >= filterAge;
+				default:
+					return null;
+				}
+			}
+
+		static Func<persons, string> GetFormatter(string format)
+		{
+			switch (format)
+			{
+				case "name": return p => $"{p.name}";
+				case "age": return p => $"{p.age}";
+				case "name age": return p => $"{p.name} - {p.age}";
+				default:
+					return null;
+			}
+		}
+		static void printpeople (persons[] people,
+					Func<persons, bool> condition, Func<persons, string> formatter)
+		{
+			foreach (var person in people)
+				{
+				if (condition(person))
+					{
+					Console.WriteLine(formatter(person));
+					}
+				}
+			}
+		}
+
+	}
