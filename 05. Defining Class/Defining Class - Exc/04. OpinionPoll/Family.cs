3971eafee2db0ea0384309d102e6d4b9a3a83288
Antoan Lukanov <77226904+Bu7eRs@users.noreply.github.com>
2021-02-07 22:17:58 +0200

    .

---
+++ 05. Defining Class/Defining Class - Exc/04. OpinionPoll/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+   public class Family
+    {
+
+        public Family()
+        {
+                this.Members = new List<Person>();
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person person)
+        {
+            this.Members.Add(person);
+        }
+
+        public void GetOldestMember()
+
+        {
+            foreach (var member in Members.OrderBy(x => x.Name).Where(x => x.Age > 30))
+            {
+                Console.WriteLine($"{member.Name} - {member.Age}");
+            }
+        }
+    }
+}
---
+++ 05. Defining Class/Defining Class - Exc/04. OpinionPoll/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person currentPerson = new Person(name, age);
+
+                family.AddMember(currentPerson);
+            }
+
+            family.GetOldestMember();
+        }
+    }
+}
