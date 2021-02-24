dec8b0e4cead1fef18c0b66b1999520fad840665
Antoan Lukanov <77226904+Bu7eRs@users.noreply.github.com>
2021-02-24 21:56:37 +0200

    update

---
+++ C# - OOP/01. Inheritance/Inheritance - Exc/05. Restaurant/Fish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Fish : MainDish
+    {
+        private const double Grams = 22;
+        public Fish(string name, decimal price)
+            : base(name, price, Grams)
+        {
+        }
+
+    }
+}
