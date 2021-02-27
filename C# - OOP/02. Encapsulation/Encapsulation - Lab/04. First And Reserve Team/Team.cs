16512475e7b90fcf9cd2fea64453566e3211c644
Antoan Lukanov <77226904+Bu7eRs@users.noreply.github.com>
2021-02-27 18:14:19 +0200

    Update.

---
+++ C# - OOP/02. Encapsulation/Encapsulation - Lab/04. First And Reserve Team/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        private string name;
+
+        public Team(string name)
+        {
+            Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+
+        }
+
+        public string Name { get; set; }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get => firstTeam.AsReadOnly();
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get => reserveTeam.AsReadOnly();
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                firstTeam.Add(player);
+            }
+            else
+            {
+                reserveTeam.Add(player);
+            }
+        }
+    }
+}
